1c8698587e5da184a8623641b221bac74e2b38ac
Usmaan Y <usmaan1901@gmail.com>
2022-08-27 17:36:44 +0100

    some more random sims
    there's also a csv

--- Sporadic Impulse 2022/OpenRocketPlotter.m
+++ Sporadic Impulse 2022/OpenRocketPlotter.m
@@ -16,13 +16,13 @@ clear; clc; close all
 % UNSELECT all comments (simulation descriptions, field descriptions and
 % flight events)
 
-filename = '';
+filename = 'data.csv';
 data = readtable(filename);
 
 data.Properties.VariableNames = {'time','altitude','vert_vel','vert_acc','tot_vel',...
     'tot_acc','pos_east','pos_north','lat_dist','lat_dir','lat_vel','lat_acc','latitude',...
     'longitude','grav_acc','aoa','roll_rate','pitch_rate','yaw_rate','mass','prop_mass',...
-    'longitudinal_moment_of_inertia','rotational_moment_of_inertia','cp','cg','stability margin',...
+    'longitudinal_moment_of_inertia','rotational_moment_of_inertia','cp','cg','stability_margin',...
     'mach','re','thrust','drag','cd','axial_cd','friction_cd','pressure_cd','base_cd',...
     'normal_force_coeff','pitch_moment_coeff','yaw_moment_coeff','side_force_coeff',...
     'roll_moment_coeff','roll_forcing_coeff','roll_damping_coeff','pitch_damping_coeff',...
@@ -32,15 +32,31 @@ data.Properties.VariableNames = {'time','altitude','vert_vel','vert_acc','tot_ve
 % trajectory plots
 figure()
 yyaxis right
-plot(data.time,data.altitude,'-k','LineWidth',1.2);
+plot(data.time,data.altitude,'-k','LineWidth',1.5);
 hold on
-ylabel('Altitude (m)','Interpreter','latex');
+ylabel('Altitude (m)','Interpreter','latex','FontSize',14);
 yyaxis left
-plot(data.time,data.vert_vel,'-r','LineWidth',1.2);
-plot(data.time,data.vert_acc, '-g', 'LineWidth',1.2);
-xlabel('Time (s)','Interpreter','latex');
-ylabel('Vertical Velocity (m/s), Vertical Acceleration (m/s$^$2)','Interpreter','latex');
-legend({'Vertical Velocity','Vertical Acceleration','Altitude'},'Location','northeast','Interpreter','latex');
+plot(data.time,data.vert_vel,'-r','LineWidth',1.5);
+plot(data.time,data.vert_acc, '-b', 'LineWidth',1.5);
+xlabel('Time (s)','Interpreter','latex','FontSize',14);
+ylabel('Vertical Velocity (m/s), Vertical Acceleration (m/s$^2$)','Interpreter','latex','FontSize',14);
+legend({'Vertical Velocity','Vertical Acceleration','Altitude'},'Location','northeast','Interpreter','latex','FontSize',14);
+box on;
+grid on;
+hold off
+
+% 3d plot
+figure()
+yyaxis right
+plot(data.time,data.altitude,'-k','LineWidth',1.5);
+hold on
+ylabel('Altitude (m)','Interpreter','latex','FontSize',14);
+yyaxis left
+plot(data.time,data.vert_vel,'-r','LineWidth',1.5);
+plot(data.time,data.vert_acc, '-b', 'LineWidth',1.5);
+xlabel('Time (s)','Interpreter','latex','FontSize',14);
+ylabel('Vertical Velocity (m/s), Vertical Acceleration (m/s$^2$)','Interpreter','latex','FontSize',14);
+legend({'Vertical Velocity','Vertical Acceleration','Altitude'},'Location','northeast','Interpreter','latex','FontSize',14);
 box on;
 grid on;
 hold off
@@ -48,28 +64,30 @@ hold off
 % ground track plots
 figure();
 subplot(1,2,1)
-plot(data.pos_east, data.altitude,'-k','LineWidth',1.2);
-xlabel('Position East of Launch Pad (m)','Interpreter','latex');
-ylabel('Altitude (m)','Interpreter','latex');
+plot(data.pos_east, data.altitude,'-k','LineWidth',2);
+xlabel('Position East of Launch Pad (m)','Interpreter','latex','FontSize',14);
+ylabel('Altitude (m)','Interpreter','latex','FontSize',14);
+box on;
+grid on;
 subplot(1,2,2)
-plot(data.pos_east, data.pos_north, '-k','LineWidth',1.2);
-xlabel('Position East of Launch Pad (m)','Interpreter','latex');
-ylabel('Position North of Launch Pad (m)','Interpreter','latex');
+plot(data.pos_east, data.pos_north, '-k','LineWidth',2);
+xlabel('Position East of Launch Pad (m)','Interpreter','latex','FontSize',14);
+ylabel('Position North of Launch Pad (m)','Interpreter','latex','FontSize',14);
 box on;
 grid on;
 
 % stability plots
 figure();
 yyaxis right
-plot(data.time, data,'-k','LineWidth',1);
+plot(data.time, data.stability_margin,'-r','LineWidth',1.5);
 hold on
-ylabel('Stability Margin (cal)','Interpreter','latex');
+ylabel('Stability Margin (cal)','Interpreter','latex','FontSize',14);
 yyaxis left
-plot(data.time, data.cg, '-r', 'LineWidth',1);
-plot(data.time, data.cp, '-g', 'LineWidth',1);
-xlabel('Time (s)','Interpreter',"latex");
-ylabel('Centre of Gravity (cm), Centre of Pressure (cm)','Interpreter','latex');
-legend({'Centre of Gravity','Centre of Pressure','Stability Margin'},'Location','southeast','Interpreter','latex');
+plot(data.time, data.cg, '-k', 'LineWidth',1.5);
+plot(data.time, data.cp, '-b', 'LineWidth',1.5);
+xlabel('Time (s)','Interpreter','latex','FontSize',14);
+ylabel('Centre of Gravity (cm), Centre of Pressure (cm)','Interpreter','latex','FontSize',14);
+legend({'Centre of Gravity','Centre of Pressure','Stability Margin'},'Location','southeast','Interpreter','latex','FontSize',11);
 box on;
 grid on;
 hold off
