de7a22f166705198d63890526f4c0052ad7ea4d5
Usmaan Y <usmaan1901@gmail.com>
2022-09-08 15:42:45 +0100

    more sims

--- Sporadic Impulse 2022/hotfire 7 data /thrust_hotfire7.m
+++ Sporadic Impulse 2022/hotfire 7 data /thrust_hotfire7.m
@@ -2,7 +2,6 @@ clear; clc; close all
 
 filename = 'sensor_data.csv';
 data = readtable(filename);
-
 data.Properties.VariableNames = {'1','2','3','4','5','6',...
     '7','8','9','10','11','12','thrust','14','time'};
 
@@ -11,14 +10,22 @@ time = data.time(range);
 thrust = -(data.thrust(range)-7146.3)./90436;
 
 figure()
-plot(time-min(time),thrust,LineWidth=1.25)
+plot((time-min(time))./1000,thrust.*1000,LineWidth=1.25)
+xlabel('Time (s)',Interpreter='latex',FontSize=14)
+ylabel('Thrust (N)',Interpreter='latex',FontSize=14)
+set(gca,'TickLabelInterpreter','latex')
+grid on
 
 % scaled to 2kN
-thrust_scaled = (thrust./max(thrust)).*2000;
+thrust_scaled = ((thrust-min(thrust))./max(thrust)).*2000;
 time_scaled = (time-min(time))./1000;
 
 figure()
 plot(time_scaled,thrust_scaled,LineWidth=1.25)
+xlabel('Time (s)',Interpreter='latex',FontSize=14)
+ylabel('Thrust (N)',Interpreter='latex',FontSize=14)
+set(gca,'TickLabelInterpreter','latex')
+grid on
 
 % write scaled data to a file that doesn't work with openrocket :')
 A = [time_scaled'; thrust_scaled'];
@@ -28,4 +35,6 @@ fprintf(fileID,'%6s %12s\n','time (s)','thrust (N)');
 fprintf(fileID,'%6.2f %12.2f\n',A);
 fclose(fileID);
 
+
 % [min_time, index] = min(abs(time-TIME_U_WANT))
+%  ^ this bit of code is super janky but also super useful
