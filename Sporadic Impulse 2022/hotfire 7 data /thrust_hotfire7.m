64ffa52e94ab5e9b68773011852cdf03f2ec2604
Usmaan Y <usmaan1901@gmail.com>
2022-09-22 18:56:41 +0100

    rocket changes
    changes to rocket length on the recovery tube and updated thrust curve to most recent hotfire

--- Sporadic Impulse 2022/hotfire 7 data /thrust_hotfire7.m
+++ Sporadic Impulse 2022/hotfire 7 data /thrust_hotfire7.m
@@ -38,3 +38,27 @@ fclose(fileID);
 
 % [min_time, index] = min(abs(time-TIME_U_WANT))
 %  ^ this bit of code is super janky but also super useful
+grid on
+
+% scaled to 2kN
+thrust_scaled = (thrust./max(thrust)).*2000;
+time_scaled = (time-min(time))./1000;
+
+figure()
+plot(time_scaled,thrust_scaled,LineWidth=1.25)
+xlabel('Time (s)',Interpreter='latex',FontSize=14)
+ylabel('Thrust (N)',Interpreter='latex',FontSize=14)
+set(gca,'TickLabelInterpreter','latex')
+grid on
+
+% write scaled data to a file that doesn't work with openrocket :')
+A = [time_scaled'; thrust_scaled'];
+
+fileID = fopen('thrust.txt','w');
+fprintf(fileID,'%6s %12s\n','time (s)','thrust (N)');
+fprintf(fileID,'%6.2f %12.2f\n',A);
+fclose(fileID);
+
+
+% [min_time, index] = min(abs(time-TIME_U_WANT))
+%  ^ this bit of code is super janky but also super useful
